087f5a7ca5389d35bc4fee4e272ec3341689fab1
Dave Goldman <39909564+dgoldman-msft@users.noreply.github.com>
2024-11-12 21:57:00 -0500

    New Check-in
    Added debug logging
    Added dot strings

--- README.md
+++ README.md
@@ -6,10 +6,26 @@ Hash-based Message Authentication Code Generator
 This script was created in python
 
 ### DESCRIPTION
-This will allow you to create a HMAC in a selected hash type
+This script calculates HMAC (Hash-based Message Authentication Code) for a given message and key using various hash algorithms.
 
-### Example
+Functions: Calculates and prints the HMAC for the given data and key using the specified hash type.
 
-- EXAMPLE 1: python3 ./summac "Your Message" "Your Key" "Hash type"
+    sumhmac(data, key, hashtype, debug=False):
 
-    Creates a HMAC by passing in your message, key and selected hash type
+Parameters:
+
+        data (str): The message to hash.
+        key (str): The key to use for hashing.
+        hashtype (str): The hash type to use. Valid options are "MD5", "SHA1", "SHA224", "SHA256", "SHA384", "SHA512", "all".
+        debug (bool): If True, enables debug logging. Default is False.
+
+Usage: Run the script from the command line with the following arguments:
+
+    message (str): The message to hash.
+    key (str): The key to use for hashing.
+    hashtype (str): The hash type to use. Valid options are "MD5", "SHA1", "SHA224", "SHA256", "SHA384", "SHA512", "all".
+    -dbg, --debug (optional): Enable debug logging.
+
+Example:
+
+    python sumhmac.py "Hello, World!" "secret_key" "SHA256"
--- hashsum.py
+++ hashsum.py
@@ -1,43 +1,98 @@
+# Define interpreter
 #!/usr/bin/env python3
+
+# Imported libraries
+import argparse
 import hashlib
-import sys
+import hmac
+from colorama import Fore, Style, init
 
-def hashsum(data, hashtype):
-    def switch_case(hashtype):
-    if hashtype.lower() == "md5" or hashtype.upper() == "MD5":
-	    return hashlib.md5(data.encode()).hexdigest()
-	elif hashtype.lower() == "sha1" or hashtype.upper() == "SHA1":
-            return hashlib.sha1(data.encode()).hexdigest()
-	elif hashtype.lower() == "sha224" or hashtype.upper() == "SHA224":
-            return hashlib.sha224(data.encode()).hexdigest()
-	elif hashtype.lower() == "sha256" or hashtype.upper() == "SHA256":
-            return hashlib.sha256(data.encode()).hexdigest()
-	elif hashtype.lower() == "sha512" or hashtype.upper() == "SHA512":
-            return hashlib.sha512(data.encode()).hexdigest()
-	elif hashtype == "all":
-            md5sum = hashlib.md5(data.encode()).hexdigest()
-            sha1sum = hashlib.sha1(data.encode()).hexdigest()
-            sha224sum = hashlib.sha224(data.encode()).hexdigest()
-            sha256sum = hashlib.sha256(data.encode()).hexdigest()
-            sha512sum = hashlib.sha512(data.encode()).hexdigest()
-            return md5sum, sha1sum, sha224sum, sha256sum, sha512sum
-        else:
-            return "You did not specify a valid hash type to return!"
+# Initialize colorama
+init(autoreset=True)
 
-    # Call the switch_case function
-    return switch_case(hashtype)
+# Function to calculate HMAC's
+def hashsum(data, key, hashtype, debug=False):
+    if debug:
+        print(f"{Fore.RED}Calling display_and_select{Style.RESET_ALL}")
 
-# Call the function with a message and a hash type from command line arguments
-if len(sys.argv) != 3:
-    print("Usage: python hashsum.py <message> <hashtype>")
-    print("hash types: MD5, SHA1, SHA224, SHA256, SHA512, all")
-else:
-    print("Creating hashes...")
-    message = sys.argv[1]
-    hashtype = sys.argv[2]
-    result = hashsum(message, hashtype)
-    if hashtype == "all":
-        for hash_type, hash_value in zip(["MD5", "SHA1", "SHA224", "SHA256", "SHA512"], result):
-            print(f"HASH ({hash_type}): {hash_value}")
-    else:
-        print(f"HASH ({hashtype}): {result}")
+    # Convert the key and data to bytes if they are not already
+    if not isinstance(key, bytes):
+        if debug:
+            print(f"{Fore.RED}Converting key to bytes{Style.RESET_ALL}")
+        key = key.encode()
+    if not isinstance(data, bytes):
+        data = data.encode()
+
+    # Nested function to switch between different hash types
+    def switch(hashtype):
+        if debug:
+            print(f"{Fore.RED}Selecting Hash Type{Style.RESET_ALL}")
+
+        # Using if-elif-else to determine the hash type due to backwards compatibility with Python 3.7 and earlier
+        try:
+            if hashtype == "MD5":
+                return print(f"{Fore.GREEN}HMAC (MD5): {Fore.YELLOW}" + hmac.new(key, data, hashlib.md5).hexdigest() + f"{Style.RESET_ALL}")
+            elif hashtype == "SHA1":
+                return print(f"{Fore.GREEN}HMAC (SHA1): {Fore.YELLOW}" + hmac.new(key, data, hashlib.sha1).hexdigest() + f"{Style.RESET_ALL}")
+            elif hashtype == "SHA224":
+                return print(f"{Fore.GREEN}HMAC (SHA224): {Fore.YELLOW}" + hmac.new(key, data, hashlib.sha224).hexdigest() + f"{Style.RESET_ALL}")
+            elif hashtype == "SHA256":
+                return print(f"{Fore.GREEN}HMAC (SHA256): {Fore.YELLOW}" + hmac.new(key, data, hashlib.sha256).hexdigest() + f"{Style.RESET_ALL}")
+            elif hashtype == "SHA384":
+                return print(f"{Fore.GREEN}HMAC (SHA384): {Fore.YELLOW}" + hmac.new(key, data, hashlib.sha384).hexdigest() + f"{Style.RESET_ALL}")
+            elif hashtype == "SHA512":
+                return print(f"{Fore.GREEN}HMAC (SHA512): {Fore.YELLOW}" + hmac.new(key, data, hashlib.sha512).hexdigest() + f"{Style.RESET_ALL}")
+            elif hashtype == "all":
+                # Calculate and print all hash types
+                mdh5hash = print(f"{Fore.GREEN}HMAC (MD5): {Fore.YELLOW}" + hmac.new(key, data, hashlib.md5).hexdigest() + f"{Style.RESET_ALL}")
+                sha1hash = print(f"{Fore.GREEN}HMAC (SHA1): {Fore.YELLOW}" + hmac.new(key, data, hashlib.sha1).hexdigest() + f"{Style.RESET_ALL}")
+                sha224hash = print(f"{Fore.GREEN}HMAC (SHA224): {Fore.YELLOW}" + hmac.new(key, data, hashlib.sha224).hexdigest() + f"{Style.RESET_ALL}")
+                sha256hash = print(f"{Fore.GREEN}HMAC (SHA256): {Fore.YELLOW}" + hmac.new(key, data, hashlib.sha256).hexdigest() + f"{Style.RESET_ALL}")
+                sha384hash = print(f"{Fore.GREEN}HMAC (SHA384): {Fore.YELLOW}" + hmac.new(key, data, hashlib.sha384).hexdigest() + f"{Style.RESET_ALL}")
+                sha512hash = print(f"{Fore.GREEN}HMAC (SHA512): {Fore.YELLOW}" + hmac.new(key, data, hashlib.sha512).hexdigest() + f"{Style.RESET_ALL}")
+                return mdh5hash, sha1hash, sha224hash, sha256hash, sha384hash, sha512hash
+            else:
+                # Default case if no valid hash type is provided
+                return print(f"{Fore.RED}Valid hash types: mdh5, sha1, sha224, sha256, sha384, sha512, all{Style.RESET_ALL}")
+        except Exception as e:
+            print(f"{Fore.RED}An error occurred: {e}{Style.RESET_ALL}")
+
+    # Call the switch function to process the HMAC
+    switch(hashtype)
+
+# Check command line arguments and call the function
+if __name__ == "__main__":
+    """
+    This script calculates HMAC (Hash-based Message Authentication Code) for a given message and key using various hash algorithms.
+
+    Functions:
+        hashsum(data, key, hashtype, debug=False):
+
+        Calculates and prints the HMAC for the given data and key using the specified hash type.
+
+        Parameters:
+            data (str): The message to hash.
+            key (str): The key to use for hashing.
+            hashtype (str): The hash type to use. Valid options are "MD5", "SHA1", "SHA224", "SHA256", "SHA384", "SHA512", "all".
+            debug (bool): If True, enables debug logging. Default is False.
+    Usage:
+
+        Run the script from the command line with the following arguments:
+            message (str): The message to hash.
+            key (str): The key to use for hashing.
+            hashtype (str): The hash type to use. Valid options are "MD5", "SHA1", "SHA224", "SHA256", "SHA384", "SHA512", "all".
+            -dbg, --debug (optional): Enable debug logging.
+
+    Example:
+        python hashsum.py "Hello, World!" "secret_key" "SHA256"
+    """
+    parser = argparse.ArgumentParser(description="Calculate HMAC for a given message and key.")
+    parser.add_argument("message", type=str, help="The message to hash.")
+    parser.add_argument("key", type=str, help="The key to use for hashing.")
+    parser.add_argument("hashtype", type=str, choices=["MD5", "SHA1", "SHA224", "SHA256", "SHA384", "SHA512", "all"], help="The hash type to use.")
+
+    debug_group = parser.add_argument_group('Debug options')
+    debug_group.add_argument("-dbg", "--debug", action="store_true", help="Enable debug logging")
+
+    args = parser.parse_args()
+    hashsum(args.message, args.key, args.hashtype)
